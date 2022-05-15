7c52516304460afd9eb166e59912d9fe547aa4a2
LSHristova20 <LSHristova20@codingburgas.bg>
2022-05-15 21:15:28 +0300

    adding menu to the code
    adding menu

--- LogicGame.cpp
+++ LogicGame.cpp
@@ -12,3 +12,13 @@ int main()
 	cout << "===================================================== \n";
 	cout << " \t\tLogic game project\t \n ";
 	cout << "===================================================== \n";
+	
+		// The menu
+	while (calcOn != false) {
+		cout << "*******************************\n";
+		cout << " 1 - Our Team.\n";
+    cout << " 2 - Guide.\n";
+    cout << " 3 - Logic game\n";
+    cout << " 4 - Exit.\n";
+    cout << "*******************************\n";
+		cout << " Enter your choice and press return: ";
