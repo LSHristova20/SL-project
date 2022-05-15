9b30831019f92b734a8f669e7d59742e7228d1c1
LSHristova20 <LSHristova20@codingburgas.bg>
2022-05-15 21:16:16 +0300

    writing the code

--- LogicGame.cpp
+++ LogicGame.cpp
@@ -17,8 +17,10 @@ int main()
 	while (calcOn != false) {
 		cout << "*******************************\n";
 		cout << " 1 - Our Team.\n";
-    cout << " 2 - Guide.\n";
-    cout << " 3 - Logic game\n";
-    cout << " 4 - Exit.\n";
-    cout << "*******************************\n";
+		cout << " 2 - Guide.\n";
+		cout << " 3 - Logic game\n";
+		cout << " 4 - Exit.\n";
+		cout << "*******************************\n";
 		cout << " Enter your choice and press return: ";
+		
+		cin >> choice;
