45780fec5b03726dc301f65dfc6b1419b6da92b0
LSHristova20 <LSHristova20@codingburgas.bg>
2022-05-15 21:13:50 +0300

    writing the code

--- LogicGame.cpp
+++ LogicGame.cpp
@@ -1,1 +1,14 @@
 #include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int choice;
+	bool calcOn = true;
+	char op;
+	float num1, num2;
+	// The title
+	cout << "===================================================== \n";
+	cout << " \t\tLogic game project\t \n ";
+	cout << "===================================================== \n";
