9a1af644ca25ecf855d7ae3c46f6add339794c62
LSHristova20 <LSHristova20@codingburgas.bg>
2022-05-15 23:41:26 +0300

    Update LogicGame.cpp

--- LogicGame.cpp
+++ LogicGame.cpp
@@ -5,17 +5,16 @@ using namespace std;
 int main()
 {
 	int choice;
-	bool calcOn = true;
+	bool log = true;
 	char op;
 	float num1, num2;
+	
 	// The title
 	cout << "===================================================== \n";
 	cout << " \t\tLogic game project\t \n ";
 	cout << "===================================================== \n";
-	
 	// The menu
-	while (calcOn != false) {
-		
+	while (log != false) {
 		cout << "*******************************\n";
 		cout << " 1 - Our Team.\n";
 		cout << " 2 - Guide.\n";
@@ -23,56 +22,57 @@ int main()
 		cout << " 4 - Exit.\n";
 		cout << "*******************************\n";
 		cout << " Enter your choice and press return: ";
-		
+
 		cin >> choice;
-		
+
 		switch (choice)
 		{
-			case 1:
-				// More about our team
-			cout << "---------------------------------------------------------- \n";
-			cout << "Our team:\n";
-			cout << "---------------------------------------------------------- \n";
-			cout << "Lorena Hristova - Leader and Scrum trainer\n";
-			cout << "---------------------------------------------------------- \n";
-			cout << "Lorena Hristova and Svetoslav Stoilov - Back end Developers\n";
-			cout << "---------------------------------------------------------- \n";
-			cout << "Lorena Hristova - QA engineer\n";
-			cout << "---------------------------------------------------------- \n";
+		case 1:
+			// More about our team
+		cout << "---------------------------------------------------------- \n";
+		cout << "Our team:\n";
+		cout << "---------------------------------------------------------- \n";
+		cout << "Lorena Hristova - Leader and Scrum trainer\n";
+		cout << "---------------------------------------------------------- \n";
+		cout << "Lorena Hristova and Svetoslav Stoilov - Back end Developers\n";
+		cout << "---------------------------------------------------------- \n";
+		cout << "Lorena Hristova - QA engineer\n";
+		cout << "---------------------------------------------------------- \n";
 				
-			break;
-				
-			case 2:
-			// This is the guide for the application
-			cout << endl;
-			cout << "To play the logic game you just have to...\n";
-			cout << endl;
+		break;
+      
+		case 2:
+		// This is the guide for the application
+		cout << endl;
+		cout << "To play the logic game you just have to...\n";
+		cout << endl;
+		
+		break;
+
+		case 3:
+        cout << endl;
+		cout << "To be continued...\n";
+        cout << endl;
 			
-			break;
-				
-			case 3:
-			cout << endl;
-			cout << "To be continued...\n";
-			cout << endl;
-				
-			break;
-				
-			case 4:
-			// The exit of the program
-			cout << "End of Program.\n";
-			calcOn = false;
+		break;
 			
-			break;
-				
-			// if you write a number above 4 it will show "Not a Valid Choice." and "Choose again."
-			default:
-			cout << "Not a Valid Choice. \n";
-			cout << "Choose again.\n";
-			cin >> choice;
-			
-			break;
-		}
+		case 4:
+		// The exit of the program
+		cout << "End of Program.\n";
+		log = false;
+		
+		break;
+		
+		// if you write a number above 4 it will show "Not a Valid Choice." and "Choose again."
+		default:
+		cout << "Not a Valid Choice. \n";
+		cout << "Choose again.\n";
+		cin >> choice;
+		
+		break;
 	}
+
+}
 	//The end of the program
 	return 0;
 }
